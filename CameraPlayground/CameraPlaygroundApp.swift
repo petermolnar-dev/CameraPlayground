fe5d257e733338ad817d1cc0c56a7707db2beb8d
Peter Molnar <petermolnar.hu@gmail.com>
2024-11-22 09:55:08 +0100

    Initial Commit

---
+++ CameraPlayground/CameraPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPlaygroundApp.swift
+//  CameraPlayground
+//
+//  Created by Peter Molnar on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CameraPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
